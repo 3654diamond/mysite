46cbd1c06c9b6e0ee739ce84ef464c4e2d151112
Mahesh Shingane <shingane@Maheshs-Laptop.Dlink>
2024-01-08 21:13:14 +0530

    r

--- mapit.py
+++ mapit.py
@@ -12,5 +12,5 @@ webbrowser.open('https://www.google.com/maps/place/' + address)
 
 print(" let us check if this is working")
 
-#Checking the the pull request of the code 
+#Checking the the pull request final
 
